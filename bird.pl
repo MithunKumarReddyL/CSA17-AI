01159a4bfa7be47b96058642d04f63f7703b6134
MithunKumarReddyL <166578052+MithunKumarReddyL@users.noreply.github.com>
2025-07-08 09:36:52 +0530

    Add files via upload

---
+++ bird.pl
@@ -0,0 +1,5 @@
+can_fly(pegion).
+can_fly(eagle).
+can_fly(parrot).
+cannot_fly(ostrich).
+cannot_fly(penguin).
---
+++ db.pl
@@ -0,0 +1,5 @@
+person(john, 15, april, 1995).
+person(alice, 22, july, 1998).
+person(mithun, 10, june, 2002).
+person(ravi, 5, january, 1990).
+person(sneha, 29, february, 1996).
